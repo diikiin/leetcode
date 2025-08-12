56c3767151d90dda9f3bfa4c78fa1aa8d78de95d
dikin <dauren280601@gmail.com>
2025-08-12 23:38:03 +0500

    1 two sum

--- src/two_sum/Solution.java
+++ src/two_sum/Solution.java
@@ -21,10 +21,32 @@ class Solution {
         return indexes;
     }
 
+    public int[] twoSumBest(int[] nums, int target) {
+        Map<Integer, Integer> sums = new HashMap<>();
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+            if (sums.containsKey(nums[start])) {
+                return new int[]{sums.get(nums[start]), start};
+            }
+            sums.put(target - nums[start], start);
+
+            if (sums.containsKey(nums[end])) {
+                return new int[]{sums.get(nums[end]), end};
+            }
+            sums.put(target - nums[end], end);
+            start++;
+            end--;
+        }
+
+        return null;
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] nums = new int[]{0, 1, 2, 3};
 
         System.out.println(Arrays.toString(s.twoSum(nums, 3)));
+        System.out.println(Arrays.toString(s.twoSumBest(nums, 3)));
     }
 }
