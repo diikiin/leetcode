d4c59ea7e0ba32ffee2a25c1eafd7dbe41ed002f
dikin <daurenkab@freedombank.kz>
2025-11-05 09:52:58 +0500

    69 sqrt(x) binary search

--- src/p69_sqrt_x/Solution.java
+++ src/p69_sqrt_x/Solution.java
@@ -10,6 +10,24 @@ public class Solution {
         }
     }
 
+    public int mySqrtBest(int x) {
+        if (x < 2) return x;
+
+        int left = 1, right = x / 2, ans = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long sq = (long) mid * mid;
+
+            if (sq == x) return mid;
+            else if (sq < x) {
+                ans = mid;
+                left = mid + 1;
+            } else right = mid - 1;
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println(s.mySqrt(4));
@@ -17,5 +35,11 @@ public class Solution {
         System.out.println(s.mySqrt(9));
         System.out.println(s.mySqrt(256));
         System.out.println(s.mySqrt(590));
+
+        System.out.println(s.mySqrtBest(4));
+        System.out.println(s.mySqrtBest(8));
+        System.out.println(s.mySqrtBest(9));
+        System.out.println(s.mySqrtBest(256));
+        System.out.println(s.mySqrtBest(590));
     }
 }
