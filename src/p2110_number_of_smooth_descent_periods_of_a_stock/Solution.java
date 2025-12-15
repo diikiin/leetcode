84e3bbabc65d3e3184f04363189b225482b18a85
dikin <daurenkab@freedombank.kz>
2025-12-15 14:39:02 +0500

    2110 number of smooth descent periods of a stock

---
+++ src/p2110_number_of_smooth_descent_periods_of_a_stock/Solution.java
@@ -0,0 +1,41 @@
+package p2110_number_of_smooth_descent_periods_of_a_stock;
+
+public class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long res = prices.length;
+        long streak = 0;
+        for (int i = 1; i <= prices.length; i++) {
+            if (i < prices.length && prices[i - 1] - prices[i] == 1) streak++;
+            else {
+                res += (streak * (streak + 1)) / 2;
+                streak = 0;
+            }
+        }
+        return res;
+    }
+
+    public long getDescentPeriodsBest(int[] prices) {
+        long res = 0;
+        int prev = 1;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] == prices[i - 1] - 1) prev++;
+            else prev = 1;
+            res += prev;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.getDescentPeriods(new int[]{3, 2, 1, 4}));
+        System.out.println(s.getDescentPeriods(new int[]{8, 6, 7, 7}));
+        System.out.println(s.getDescentPeriods(new int[]{1}));
+        System.out.println(s.getDescentPeriods(new int[]{12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 4, 3, 10, 9, 8, 7}));
+
+        System.out.println(s.getDescentPeriodsBest(new int[]{3, 2, 1, 4}));
+        System.out.println(s.getDescentPeriodsBest(new int[]{8, 6, 7, 7}));
+        System.out.println(s.getDescentPeriodsBest(new int[]{1}));
+        System.out.println(s.getDescentPeriodsBest(new int[]{12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 4, 3, 10, 9, 8, 7}));
+    }
+}
