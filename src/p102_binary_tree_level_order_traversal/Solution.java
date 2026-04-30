c939b4dc245380a0d05e1c79c2da7243e9f94c83
dikin <daurenkab@freedombank.kz>
2026-04-30 16:37:34 +0500

    102 binary tree level order traversal

---
+++ src/p102_binary_tree_level_order_traversal/Solution.java
@@ -0,0 +1,41 @@
+package p102_binary_tree_level_order_traversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+
+            result.add(level);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.levelOrder(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))));
+        System.out.println(s.levelOrder(new TreeNode(1)));
+        System.out.println(s.levelOrder(null));
+    }
+}
